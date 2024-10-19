d6419489c2d8795515a7c5fca8f8b8eadceefa77
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2024-10-19 17:11:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-export default {
-  build: {
-    rollupOptions: {
-      external: ['/src/main.jsx']
-    }
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+})
+// export default {
+//   build: {
+//     rollupOptions: {
+//       external: ['/src/main.jsx']
+//     }
+//   }
+// }
