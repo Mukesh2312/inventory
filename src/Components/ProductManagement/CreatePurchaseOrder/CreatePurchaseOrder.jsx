88cc11e3eed54fad5a1d302479315f170456ecdf
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2024-10-21 10:31:26 +0530

    Update CreatePurchaseOrder.jsx

--- src/Components/ProductManagement/CreatePurchaseOrder/CreatePurchaseOrder.jsx
+++ src/Components/ProductManagement/CreatePurchaseOrder/CreatePurchaseOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import stl from "./createPurchaseOrder.module.css";
+import stl from "./CreatePurchaseOrder.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import CloseIcon from "../../IconComponents/CloseIcon";
 import PlusCircleIcon from "../../IconComponents/PlusCircleIcon";
